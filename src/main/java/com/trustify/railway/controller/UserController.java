b2566db95c86b835c92e4ad8f3499d694b7553fa
Moriba Hebelamou <dev.hebelamou@gmail.com>
2024-04-08 04:22:21 +0000

    Changement de port

--- src/main/java/com/trustify/railway/controller/UserController.java
+++ src/main/java/com/trustify/railway/controller/UserController.java
@@ -5,7 +5,6 @@ import com.trustify.railway.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
